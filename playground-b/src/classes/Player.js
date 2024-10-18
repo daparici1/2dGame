3516f5ff4ed397e5ba8bd7f013924693fea4ccac
David Aparicio <davidaparicioj123@gmail.com>
2024-10-18 15:16:57 -0700

    Added gravity

--- playground-b/src/classes/Player.js
+++ playground-b/src/classes/Player.js
@@ -1,32 +1,37 @@
-
 export default class Player {
-    constructor() {
-      this.position = {
-        x: 100,
-        y: 100,
-      };
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
 
-      this.width = 100;
-      this.height = 100;
-      this.sides = {
-        bottom: this.position.y + this.height,
-      };
-    }
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
 
-    draw(context) {
-      context.fillStyle = "red";
-      context.fillRect(
-        this.position.x,
-        this.position.y,
-        this.width,
-        this.height
-      );
-    }
+    this.width = 100;
+    this.height = 100;
+    this.sides = {
+      bottom: this.position.y + this.height,
+    };
+    this.gravity = 0.1;
+  }
 
-    update(canvas) {
-      if (this.sides.bottom < canvas.height) {
-        this.position.y++;
-        this.sides.bottom = this.position.y + this.height;
-      }
+  draw(context) {
+    context.fillStyle = "red";
+    context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(canvas) {
+    this.position.y += this.velocity.y;
+
+    // above bottom of canvas
+    if (this.sides.bottom + this.velocity.y < canvas.height) {
+      this.velocity.y += this.gravity;
+      this.sides.bottom = this.position.y + this.height;
+    } else {
+      this.velocity.y = 0;
     }
   }
+}
